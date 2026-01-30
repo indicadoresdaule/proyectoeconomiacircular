ed033475cef60705831163d1a031f960f9b8fe72
indicadoresdaule <indicadoresdaule@gmail.com>
2026-01-29 23:26:54 -0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/api/", "/admin/"],
       },
     ],
-    sitemap: "https://residuosdaule.vercel.app/sitemap.xml",
+    sitemap: "https://proyectoeconomiacircular.vercel.app/sitemap.xml",
   }
 }
